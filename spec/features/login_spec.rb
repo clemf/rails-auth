c83d9832be09345cc500cadedcdaeb8330679af0
Clem Freeman <clemfree@gmail.com>
2015-03-19 15:46:11 -0700

    Add feature spec for bad credential login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -14,4 +14,13 @@ feature 'the login process' do
     click_button 'Log in'
     expect(page).to have_content 'You\'ve been logged in.'
   end
+
+  scenario 'returns error for bad credentials' do
+    visit root_path
+    click_link 'Log in'
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: 'wrong'
+    click_button 'Log in'
+    expect(page).to have_content 'There was a problem logging you in.'
+  end
 end
