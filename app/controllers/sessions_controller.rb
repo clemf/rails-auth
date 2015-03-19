7dea90c9573640223c1cfc55cdcb8ba8a27860df
Clem Freeman <clemfree@gmail.com>
2015-03-19 11:22:42 -0700

    Add sessions create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.authenticate(params[:email], params[:password])
+    if @user
+      flash[:notice] = 'You\'ve been logged in.'
+      session[:user_id] = @user.id
+      redirect to root_path
+    else
+      flash[:alert] = 'There was a problem loggin you in.'
+      redirect_to log_in_path
+    end
   end
 
   def destroy
