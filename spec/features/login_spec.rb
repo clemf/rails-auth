80f0dc73177efb1c3d3b041bd04eb3388586d0f0
Clem Freeman <clemfree@gmail.com>
2015-03-19 15:43:04 -0700

    Add feature spec for credentialed login

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+
+feature 'the login process' do
+  before :each do
+    User.create(email: 'test@test.com', password: 'password')
+  end
+
+  scenario 'logs in user with correct credentials' do
+    visit root_path
+    click_link 'Log in'
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_content 'You\'ve been logged in.'
+  end
+end
