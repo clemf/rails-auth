090aeaf6db43793879b27eefa930fb0904e4d7b9
Clem Freeman <clemfree@gmail.com>
2015-03-19 11:01:02 -0700

    Add tests for user authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 describe User do
   it { should validate_confirmation_of :password }
+
+  describe '.authenticate' do
+
+    context 'when password matches hash' do
+      it 'returns the user' do
+        User.create(email: 'test@test.com', password: 'password')
+        expect(User.authenticate('test@test.com', 'password')).not_to eq nil
+      end
+    end
+
+    context 'when password does not match hash' do
+      it 'returns nil' do
+        User.create(email: 'test@test.com', password: 'password')
+        expect(User.authenticate('test@test.com', 'notpassword')).to eq nil
+      end
+    end
+    
+  end
 end
